2966f7acbd04e1715dc27f019eb0c097701f9298
Alban BOITIER <aboitier@e2r9p10.42.fr>
2019-05-07 21:28:38 +0200

    more

--- ft_strclen.c
+++ ft_strclen.c
@@ -1,34 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcdup.c                                       :+:      :+:    :+:   */
+/*   ft_strclen.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aboitier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/29 16:34:34 by aboitier          #+#    #+#             */
-/*   Updated: 2018/12/29 19:02:00 by aboitier         ###   ########.fr       */
+/*   Created: 2018/12/29 16:47:26 by aboitier          #+#    #+#             */
+/*   Updated: 2018/12/29 16:52:47 by aboitier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strcdup(const char *s, char c)
+size_t	ft_strclen(const char *s, char c)
 {
-	size_t		i;
-	size_t		j;
-	char		*new;
+	int i;
 
 	i = 0;
-	j = 0;
-	while (s[i] != c && s[i])
-		i++;
-	if (!(new = (char *)malloc(sizeof(char) * (i + 1))))
-		return (NULL);
-	while (j < i)
-	{
-		new[j] = s[j];
-		j++;
-	}
-	new[j] = '\0';
-	return (new);
+	if (s)
+		while (s[i] != c && s[i])
+			i++;
+	return (i);
 }
