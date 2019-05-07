44ed122a5679cca7de80697b49937cb0592042aa
Alban BOITIER <aboitier@e2r9p10.42.fr>
2019-05-07 20:44:51 +0200

    first commit

---
+++ ft_copyuntil.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_copyuntil.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboitier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/11 22:06:18 by aboitier          #+#    #+#             */
+/*   Updated: 2018/12/11 22:06:36 by aboitier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int			ft_copyuntil(char **dst, char *src, char c)
+{
+	int		i;
+	int		count;
+	int		pos;
+
+	i = -1;
+	count = 0;
+	while (src[++i])
+		if (src[i] == c)
+			break ;
+	pos = i;
+	if (!(*dst = ft_strnew(i)))
+		return (0);
+	while (src[count] && count < i)
+	{
+		if (!(*dst = ft_strjoinch(*dst, src[count])))
+			return (0);
+		count++;
+	}
+	return (pos);
+}
---
+++ ft_get_numbers.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_numbers.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboitier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/07 20:42:45 by aboitier          #+#    #+#             */
+/*   Updated: 2019/05/07 20:43:07 by aboitier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int     get_numbers(char *str, int nb_numbers)
+{
+	int numbers;
+	int i;
+
+	numbers = 0;
+	i = 0;
+	if (!str)
+		return (-1);
+	while (str[i] && numbers < nb_numbers)
+	{
+		while (str[i] && str[i] == ' ')
+			i++;
+		if (str[i] && str[i] == '-')
+			i++;
+		if (ft_isdigit(str[i]) > 0)
+			numbers++;
+		while (str[i] && ft_isalnum(str[i]) > 0)
+			i++;
+	}
+	return (i);
+}
+
